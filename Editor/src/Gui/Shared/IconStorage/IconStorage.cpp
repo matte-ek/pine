b8c485584f0a15f20e7bf779e6c80970bef96511
matte-ek <matero3210@gmail.com>
2024-02-04 03:54:39 +0100

    Add more icons to editor

--- Editor/src/Gui/Shared/IconStorage/IconStorage.cpp
+++ Editor/src/Gui/Shared/IconStorage/IconStorage.cpp
@@ -1,97 +1,121 @@
 #include "IconStorage.hpp"
 
 #include "Pine/Assets/Assets.hpp"
+#include "Pine/Graphics/Interfaces/IFrameBuffer.hpp"
+#include "Pine/Graphics/Graphics.hpp"
 
 namespace
 {
 
-	enum class IconType
-	{
-		Static,
-		Dynamic
-	};
+    enum class IconType
+    {
+        Static,
+        Dynamic
+    };
 
-	struct Icon
-	{
-		std::string Path;
+    struct Icon
+    {
+        std::string Path;
 
-		Pine::IAsset* Asset = nullptr;
+        Pine::IAsset *Asset = nullptr;
 
-		Pine::Texture2D* Texture = nullptr;
+        Pine::Graphics::IFrameBuffer *FrameBuffer = nullptr;
+        Pine::Texture2D *Texture = nullptr;
 
-		IconType Type = IconType::Static;
-	};
+        IconType Type = IconType::Static;
+    };
 
-	Pine::Texture2D* GetStaticIconFromAsset(Pine::IAsset* asset)
-	{
-		switch (asset->GetType())
-		{
-		case Pine::AssetType::Texture2D:
-			return dynamic_cast<Pine::Texture2D*>(asset);
-		case Pine::AssetType::Tileset:
-			return Pine::Assets::Get<Pine::Texture2D>("editor/icons/tile-set.png");
-		case Pine::AssetType::Tilemap:
-			return Pine::Assets::Get<Pine::Texture2D>("editor/icons/tile-map.png");
-		default:
-			return nullptr;
-		}
-	}
+    Pine::Texture2D *GetStaticIconFromAsset(Pine::IAsset *asset)
+    {
+        switch (asset->GetType())
+        {
+            case Pine::AssetType::Texture2D:
+                return dynamic_cast<Pine::Texture2D *>(asset);
+            case Pine::AssetType::Tileset:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/tile-set.png");
+            case Pine::AssetType::Tilemap:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/tile-map.png");
+            case Pine::AssetType::Model:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/model.png");
+            case Pine::AssetType::Level:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/level.png");
+            case Pine::AssetType::Font:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/font.png");
+            case Pine::AssetType::Shader:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/shader.png");
+            case Pine::AssetType::Blueprint:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/blueprint.png");
+            case Pine::AssetType::Material:
+                return Pine::Assets::Get<Pine::Texture2D>("editor/icons/material.png");
+            default:
+                return nullptr;
+        }
+    }
 
-	std::unordered_map<std::string, Icon> m_IconCache;
+    std::unordered_map<std::string, Icon> m_IconCache;
+    Pine::Graphics::IFrameBuffer *m_PreviewFrameBuffer = nullptr;
 
+}
+
+void IconStorage::Setup()
+{
+    m_PreviewFrameBuffer = Pine::Graphics::GetGraphicsAPI()->CreateFrameBuffer();
+    m_PreviewFrameBuffer->Create(512, 512, Pine::Graphics::Buffers::ColorBuffer);
 }
 
 void IconStorage::Update()
 {
-	std::vector<std::string> removeList;
+    std::vector<std::string> removeList;
 
-	// Find and remove unloaded assets from the icon cache
-	for (const auto& [iconAssetPath, icon] : m_IconCache)
-	{
-		if (Pine::Assets::Get(iconAssetPath))
-		{
-			continue;
-		}
+    // Find and remove unloaded assets from the icon cache
+    for (const auto &[iconAssetPath, icon]: m_IconCache)
+    {
+        auto asset = Pine::Assets::Get(iconAssetPath);
 
-		if (icon.Type == IconType::Dynamic)
-		{
-			// TODO: Remove generated textures and whatnot.
-		}
+        if (asset && !asset->IsDeleted())
+        {
+            continue;
+        }
 
-		removeList.push_back(iconAssetPath);
-	}
+        if (icon.Type == IconType::Dynamic)
+        {
+            // TODO: Remove generated textures and whatnot.
+        }
 
-	for (const auto& icon : removeList)
-	{
-		m_IconCache.erase(icon);
-	}
+        removeList.push_back(iconAssetPath);
+    }
 
-	// Generate icons
-	for (const auto& [path, asset] : Pine::Assets::GetAll())
-	{
-		Icon* icon = nullptr;
+    for (const auto &icon: removeList)
+    {
+        m_IconCache.erase(icon);
+    }
 
-		icon = &m_IconCache[path];
+    // Generate icons
+    for (const auto &[path, asset]: Pine::Assets::GetAll())
+    {
+        Icon *icon = nullptr;
 
-		// If path is empty, it has just been created.
-		if (icon->Path.empty())
-		{
-			icon->Path = path;
-			icon->Asset = asset;
-		}
+        icon = &m_IconCache[path];
 
-		icon->Texture = GetStaticIconFromAsset(asset);
-	}
+        // If path is empty, it has just been created.
+        if (icon->Path.empty())
+        {
+            icon->Path = path;
+            icon->Asset = asset;
+        }
+
+        icon->Texture = GetStaticIconFromAsset(asset);
+    }
 }
 
-Pine::Texture2D* IconStorage::GetIconTexture(const std::string& path)
+Pine::Texture2D *IconStorage::GetIconTexture(const std::string &path)
 {
-	static auto invalidAssetIcon = Pine::Assets::Get<Pine::Texture2D>("editor/icons/file.png");
+    static auto invalidAssetIcon = Pine::Assets::Get<Pine::Texture2D>("editor/icons/file.png");
 
-	if (!m_IconCache.count(path) || !m_IconCache[path].Texture)
-	{
-		return invalidAssetIcon;
-	}
+    if (!m_IconCache.count(path) || !m_IconCache[path].Texture)
+    {
+        return invalidAssetIcon;
+    }
 
-	return m_IconCache[path].Texture;
+    return m_IconCache[path].Texture;
 }
--- Editor/src/Gui/Shared/IconStorage/IconStorage.hpp
+++ Editor/src/Gui/Shared/IconStorage/IconStorage.hpp
@@ -4,6 +4,7 @@
 namespace IconStorage
 {
 
+    void Setup();
 	void Update();
 
 	Pine::Texture2D* GetIconTexture(const std::string& path);
