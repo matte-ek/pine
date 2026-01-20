faba89bc17a8db15de1ab30be82594e9c0be35d3
matte-ek <49276951+matte-ek@users.noreply.github.com>
2026-01-20 21:52:08 +0100

    whoops

---
+++ Engine/src/Pine/Game/Game.cpp
@@ -0,0 +1,57 @@
+﻿#include "Game.hpp"
+
+#include <filesystem>
+
+#include "Pine/Assets/Level/Level.hpp"
+#include "Pine/Core/Log/Log.hpp"
+#include "Pine/Core/Serialization/Serialization.hpp"
+#include "Pine/Engine/Engine.hpp"
+#include "Pine/World/World.hpp"
+
+namespace
+{
+    Pine::Game::GameProperties m_GameProperties;
+}
+
+const Pine::Game::GameProperties& Pine::Game::GetGameProperties()
+{
+    return m_GameProperties;
+}
+
+void Pine::Game::Setup()
+{
+    auto json = Serialization::LoadFromFile("game.json");
+
+    if (!json.has_value())
+    {
+        if (Engine::GetEngineConfiguration().m_ProductionMode)
+        {
+            Log::Warning("Game file missing or corrupted, game might not start properly.");
+        }
+
+        return;
+    }
+
+    const auto& j = json.value();
+
+    Serialization::LoadValue(j, "name", m_GameProperties.Name);
+    Serialization::LoadValue(j, "version", m_GameProperties.Version);
+    Serialization::LoadValue(j, "author", m_GameProperties.Author);
+    Serialization::LoadValue(j, "startupLevel", m_GameProperties.StartupLevel);
+}
+
+void Pine::Game::OnStartup()
+{
+    if (m_GameProperties.StartupLevel.empty())
+    {
+        return;
+    }
+
+    auto asset = Pine::Assets::Get<Level>(m_GameProperties.StartupLevel);
+    if (!asset)
+    {
+        Log::Warning(fmt::format("Referenced startup level {} could not be found.", m_GameProperties.StartupLevel));
+    }
+
+    World::SetActiveLevel(asset);
+}
---
+++ Engine/src/Pine/Game/Game.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <string>
+
+namespace Pine::Game
+{
+    struct GameProperties
+    {
+        // Meta
+        std::string Name;
+        std::string Version;
+        std::string Author;
+
+        // Startup
+        std::string StartupLevel;
+    };
+
+    const GameProperties& GetGameProperties();
+
+    void Setup();
+    void OnStartup();
+}
